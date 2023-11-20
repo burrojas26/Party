925351d570a0a2acd53b521405d4584ad8f4e07d
burrojas26 <114428987+burrojas26@users.noreply.github.com>
2023-11-20 13:13:12 -0500

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 public class Guest {
 	String name;
 	int company;
 	int id;
+	int tableNum = -1;
+	ArrayList<Guest> allPeople;
 	
 	public Guest(String initName, int initCompany, int initId) {
 		name = initName;
@@ -11,5 +14,13 @@ public class Guest {
 	
 	public int getCompany() {
 		return company;
+	}
+	
+	public int getTable() {
+		return tableNum;
+	}
+	
+	public String toString() {
+		return "ID number: " + id + ", Name: " + name + ", company: " + company + ", Table Number: " + tableNum;
 	}
 }
