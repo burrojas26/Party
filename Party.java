180072024966c2a6dcce63dfa1d4816073676a64
burrojas26 <114428987+burrojas26@users.noreply.github.com>
2023-11-20 13:13:36 -0500

    Update Party.java

--- Party.java
+++ Party.java
@@ -1,43 +1,66 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Party {
 	
-	ArrayList<Guest> people = new ArrayList<Guest>();
+	ArrayList<String> guests = new ArrayList<String>();
+	ArrayList<Guest> finalGuests = new ArrayList<Guest>();
+	ArrayList<String> currComps = new ArrayList<String>();
+	ArrayList<Integer> companies = new ArrayList<Integer>();
+	ArrayList<Company> allCompanies = new ArrayList<Company>();
+	String currName;
+	int currComp;
+	int currId;
+	String[] currGuest;
 	
-	ArrayList<Guest>[] tables = new ArrayList<Guest>[10]
-	
-
-	public void loadPeople() {
-		//loads the people
-	}
-	
-	public void addPerson(Guest person) {
-		people.add(person);
-	}
-	
-	public void createTable() {
-		for (ArrayList<Guest> table : tables) {
-			table = new ArrayList<Guest>();
+	public ArrayList<Guest> getAllPpl() {
+		try {
+		  File myObj = new File("partyguests.txt");
+		  Scanner myReader = new Scanner(myObj);
+		  while (myReader.hasNextLine()) {
+			String data = myReader.nextLine();
+			guests.add(data);
+		  }
+		  myReader.close();
+		} 
+		catch (FileNotFoundException e) {
+		  System.out.println("An error occurred.");
+		  e.printStackTrace();
 		}
-		for (int i = 0; i < table.length; i++) {
-			for (Guest p : people) {
-				if (!contains(p, tables[i]) && tables[i].size() < 10) {
-					tables[i].add(p);
-				}
-				
-			}
+		
+		for (String i : guests) {
+			currGuest = i.split(",");
+			currName = currGuest[2] + " " + currGuest[1];
+			currComp = Integer.parseInt(currGuest[3]);
+			currId = Integer.parseInt(currGuest[0]);
+			finalGuests.add(new Guest(currName, currComp, currId));
 		}
+		return finalGuests;
 	}
 	
-	public boolean contains(Guest person, ArrayList<Guest> table) {
-		for (Guest p : table) {
-			if (p.getCompany() == person.getCompany()) {
-				return true;
-			}
+		public ArrayList<Company> getComps() {
+		try {
+		  File myObj = new File("companies.txt");
+		  Scanner myReader = new Scanner(myObj);
+		  while (myReader.hasNextLine()) {
+			String data = myReader.nextLine();
+			currComps.add(data);
+		  }
+		  myReader.close();
+		} 
+		catch (FileNotFoundException e) {
+		  System.out.println("An error occurred.");
+		  e.printStackTrace();
 		}
-		return false;
+		
+		for (int i = 1; i <= (currComps.size()/2) + 1; i++) {
+			companies.add(i);
+		}
+		for (int i = 0; i < companies.size(); i++) {
+			allCompanies.add(new Company(i+1, companies));
+		}
+		return allCompanies;
 	}
-	
 }
