9c54f732bdaafa69c587ef9e368bfe373dad275e
burrojas26 <114428987+burrojas26@users.noreply.github.com>
2023-11-20 13:14:21 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
 public class Tester {
-	
+	public static void main(String[] args) {
+		Party theParty = new Party();
+		for (Guest g : theParty.getAllPpl()) {
+			System.out.println(g.toString());
+		}
+		ArrayList<Integer> companies = theParty.getComps();
+		for (Integer i : companies) {
+			System.out.println(i);
+		}
+	}
 }
