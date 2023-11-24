b45a987b87fc43fad0884acd660c8956248fddb8
burrojas26 <114428987+burrojas26@users.noreply.github.com>
2023-11-24 12:14:05 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -2,12 +2,15 @@ import java.util.ArrayList;
 public class Tester {
 	public static void main(String[] args) {
 		Party theParty = new Party();
-		for (Guest g : theParty.getAllPpl()) {
-			System.out.println(g.toString());
-		}
-		ArrayList<Integer> companies = theParty.getComps();
-		for (Integer i : companies) {
-			System.out.println(i);
+		ArrayList<Guest> allPpl = theParty.getAllPpl();
+		ArrayList<Company> companies = theParty.getComps();
+		ArrayList<Table> allTables = theParty.configTables(10);
+		
+		for (int i = 0; i < allTables.size(); i++) {
+			System.out.println("Table " + (int)(i+1) + ": ");
+			System.out.print(allTables.get(i).getId());
+			System.out.println("Count: " + allTables.get(i).getCount());
+			System.out.println("");
 		}
 	}
 }
