e4d52921d22ed595c16759f9f923d6940735a3db
burrojas26 <114428987+burrojas26@users.noreply.github.com>
2023-11-24 12:13:43 -0500

    Update Table.java

--- Table.java
+++ Table.java
@@ -3,9 +3,64 @@ public class Table {
 
 	Guest[] table = new Guest[10];
 		
-	public void configTable(int tableNum, int startIndex, ArrayList<Company> companies) {
+	public int configTable(int tableNum, int startIndex, ArrayList<Company> companies) {
+		Guest currPerson;
 		for (int i = 0; i < table.length; i++) {
-			table[i] = companies[startIndex].getRandPerson();
+			currPerson = companies.get(startIndex).getRandPerson();
+				table[i] = currPerson;
+				currPerson.setTabNum(tableNum);
+			if (startIndex+1 >= companies.size()) {
+				startIndex = 0;
+			}
+			else {
+				startIndex++;
+			}
 		}
+		return startIndex;
+	}
+	
+	
+	public String toString() {
+		String theString = "";
+		for (int i = 0; i < table.length; i++) {
+			theString += table[i].toString() + " ";
+		}
+		return theString;
+	}
+	
+	public String namesToString() {
+		String theString = "";
+		for (int i = 0; i < table.length; i++) {
+			theString += table[i].getName() + ", ";
+		}
+		return theString;
+	}
+	
+	public String getCompanies() {
+		String theString = "";
+		int comp;
+		for (int i = 0; i < table.length; i++) {
+			comp = table[i].getCompany();
+			theString += (comp) + ", ";
+		}
+		return theString;
+	}
+	
+	public int getCount() {
+		int count = 0;
+		for (int i = 0; i < table.length; i++) {
+			count++;
+		}
+		return count;
+	}
+	
+	public String getId() {
+		String theString = "";
+		int id;
+		for (int i = 0; i < table.length; i++) {
+			id = table[i].getId();
+			theString += (id) + ", ";
+		}
+		return theString;
 	}
 }
