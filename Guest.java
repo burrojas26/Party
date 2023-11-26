3fe4208937208dd77764fc0a183b32969f32c997
burrojas26 <114428987+burrojas26@users.noreply.github.com>
2023-11-26 13:52:36 -0500

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
+
+/** 
+ * @author Jasper Burroughs 
+ * 
+ * This file declares a Guest class, which contains an id number, a name, a company, and a table number
+ * This file contains methods to get the information and set the information for a Guest
+ */
+
 public class Guest {
 	String name;
 	int company;
 	int id;
-	int tableNum = -1;
+	int tableNum;
 	ArrayList<Guest> allPeople;
 	
 	public Guest(String initName, int initCompany, int initId) {
 		name = initName;
 		company = initCompany;
 		id = initId;
+		//Table number is -1 because it has not been declared yet
+		tableNum = -1;
 	}
 	
 	public int getCompany() {
@@ -30,9 +40,18 @@ public class Guest {
 	
 	public void setTabNum(int num) {
 		tableNum = num;
+		
 	}
 	
 	public String toString() {
-		return "ID number: " + id + ", Name: " + name + ", company: " + company + ", Table Number: " + tableNum;
+		String theString = "";
+		theString += "ID number: " + id + ", Name: " + name + ", company: " + company;
+		if (tableNum != -1) {
+			theString += ", table: " + tableNum;
+		}
+		else {
+			theString += ", table: NA";
+		}
+		return theString;
 	}
 }
