5ea1ec1cda2a7b80e83f277d79e77c779f05dffa
burrojas26 <114428987+burrojas26@users.noreply.github.com>
2023-11-26 13:52:19 -0500

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,18 +1,29 @@
 import java.util.ArrayList;
+
+/**
+ * @author Jasper Burroughs
+ * 
+ * This file defines a company class, which contains a list of employees and a number representing the company
+ * 
+ * It contains a method to add people to the company
+ * It contains multiple getters to get the information from the Company class
+ */
+
 public class Company {
 	
 	ArrayList<Guest> employees = new ArrayList<Guest>();
 	ArrayList<String> currComps = new ArrayList<String>();
 	
-	ArrayList<Integer> companies;
 	int companyNum;
 	String companyName;
 	
-	public Company(int initCompNum, ArrayList<Integer> initCompanies) {
+	public Company(int initCompNum) {
 		companyNum = initCompNum;
-		companies = initCompanies;
 	}
 	
+	/*
+	 * adds all of the people to the company
+	 */
 	public void createComp(ArrayList<Guest> allPeople) {
 		for (Guest p : allPeople) {
 			if (p.getCompany() == companyNum) {
@@ -21,15 +32,9 @@ public class Company {
 		}
 	}
 	
-	public Guest getRandPerson() {
-		for (Guest e : employees) {
-			if (e.getTable() == -1) {
-				return e;
-			}
-		}
-		return new Guest("", companyNum, 0);
-	}
-	
+	/* 
+	 * converts the information to a string
+	 */
 	public String toString() {
 		String theString = "";
 		for (Guest g : employees) {
@@ -46,5 +51,12 @@ public class Company {
 		return count;
 	}
 		
+	public ArrayList<Guest> getPpl() {
+		return employees;
+	}
+	
+	public int getNum() {
+		return companyNum;
+	}
 	
 }
