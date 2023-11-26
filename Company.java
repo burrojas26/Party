ce192f7d3d2842ab38d3a40d3d58ea937d22b678
burrojas26 <114428987+burrojas26@users.noreply.github.com>
2023-11-26 18:59:36 -0500

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * @author Jasper Burroughs
+ * @since 11/20/23
  * 
  * This file defines a company class, which contains a list of employees and a number representing the company
  * 
- * It contains a method to add people to the company
+ * It contains a method to add people to the company and get the company name
  * It contains multiple getters to get the information from the Company class
  */
 
@@ -23,26 +27,54 @@ public class Company {
 	
 	/*
 	 * adds all of the people to the company
+	 * goes through the companies file and assigns the company a name based on their number
 	 */
 	public void createComp(ArrayList<Guest> allPeople) {
+		ArrayList<String> companyData = new ArrayList<String>();
 		for (Guest p : allPeople) {
 			if (p.getCompany() == companyNum) {
 				employees.add(p);
+			}
+		}
+		//finds the company name
+		//code modified from w3 schools
+		try {
+		  File myObj = new File("companies.txt");
+		  Scanner myReader = new Scanner(myObj);
+		  while (myReader.hasNextLine()) {
+			String data = myReader.nextLine();
+			companyData.add(data);
+		  }
+		  myReader.close();
+		} 
+		catch (FileNotFoundException e) {
+		  System.out.println("An error occurred.");
+		  e.printStackTrace();
+		}
+		
+		for (String line : companyData) {
+			if (!line.equals("")) {
+				if (Integer.parseInt(line.split(",")[0]) == companyNum) {
+					companyName = line.split(",")[1];
+				}
 			}
 		}
 	}
 	
 	/* 
 	 * converts the information to a string
+	 * returns the string
 	 */
 	public String toString() {
-		String theString = "";
+		String theString = companyName + ": ";
 		for (Guest g : employees) {
-			theString += g.toString() + " ";
+			theString += g.getName() + ", ";
 		}
 		return theString;
 	}
-	
+	/*
+	 * returns the number of people belonging to the company
+	 */
 	public int getCount() {
 		int count = 0;
 		for (Guest g : employees) {
@@ -50,13 +82,23 @@ public class Company {
 		}
 		return count;
 	}
-		
+	/*
+	 * returns a list of the Guests that belong to the company
+	 */	
 	public ArrayList<Guest> getPpl() {
 		return employees;
 	}
-	
+	/*
+	 * returns the number that represents the company
+	 */
 	public int getNum() {
 		return companyNum;
+	}
+	/*
+	 * returns the name of the company
+	 */
+	public String getName() {
+		return companyName;
 	}
 	
 }
