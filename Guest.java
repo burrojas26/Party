4fbf73f3a76e05353fbb48361959488dbaa8a88c
burrojas26 <114428987+burrojas26@users.noreply.github.com>
2023-11-26 19:00:02 -0500

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 
 /** 
  * @author Jasper Burroughs 
+ * @since 11/16/23
  * 
  * This file declares a Guest class, which contains an id number, a name, a company, and a table number
  * This file contains methods to get the information and set the information for a Guest
+ * This file contains a method to convert all of the person's information to a string and use a passed in list to get the name of teh company that corresponds to the number
  */
 
 public class Guest {
@@ -22,30 +24,62 @@ public class Guest {
 		tableNum = -1;
 	}
 	
+	/*
+	 * returns the number that represents the company that the Guest belongs to
+	 */
 	public int getCompany() {
 		return company;
 	}
-	
+	/*
+	 * returns the name of the company that the Guest belongs to
+	 */
+	public String getCompanyName(ArrayList<Company> companies) {
+		String theString = "";
+		for (Company c : companies) {
+			if (c.getNum() == company) {
+				theString += c.getName();
+			}
+		}
+		return theString;
+	}
+	/*
+	 * returns the table number that the Guest has been assigned to
+	 */
 	public int getTable() {
 		return tableNum;
 	}
-	
+	/*
+	 * returns the name of the Guest
+	 */
 	public String getName() {
 		return name;
 	}
-	
+	/*
+	 * returns the id of the Guest
+	 */
 	public int getId() {
 		return id;
 	}
-	
+	/*
+	 * sets the table number to a pssed in int
+	 */
 	public void setTabNum(int num) {
 		tableNum = num;
 		
 	}
 	
-	public String toString() {
+	/*
+	 * converts all of the person's information to a string
+	 * looks through a passed in list of companies to get the name of the company that the number corresponds to
+	 */
+	public String toString(ArrayList<Company> companies) {
 		String theString = "";
-		theString += "ID number: " + id + ", Name: " + name + ", company: " + company;
+		theString += "ID number: " + id + ", Name: " + name + ", company: ";
+		for (Company c : companies) {
+			if (c.getNum() == company) {
+				theString += c.getName();
+			}
+		}
 		if (tableNum != -1) {
 			theString += ", table: " + tableNum;
 		}
